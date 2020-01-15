6e2ef56f3d93065b573f560c707b71fd10e3cff9
antosha-85 <antosha-85@yandex.ru>
2020-01-15 23:07:01 +0000

    Finished with stdin setup

--- play.js
+++ play.js
@@ -3,3 +3,27 @@ const net = require('net');
 const {connect} = require('./client')
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', handleUserInput);
+    function handleUserInput (key) {
+        if (key === '\u0003') {
+            console.log(`pressed ctrc`, key)
+            process.exit();
+        } else {
+            console.log(`pressed a diff button!`);
+        }
+    }
+    return stdin;
+}
+// const handleUserInput = function (key) {
+//     if (key === '\u0003') {
+//         process.exit();
+//     } 
+//     console.log(`pressed a diff button!`);
+// }
+setupInput();
